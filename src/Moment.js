364fc22635132fc121d62699daebb20d33fa7730
mengnans <thundersmn@outlook.com>
2018-01-25 20:13:50 +1100

    small update

--- src/Moment.js
+++ src/Moment.js
@@ -1,26 +1,67 @@
 import React, {Component} from 'react';
-import {Image, StyleSheet, Text, View} from "react-native";
-import PixelRatio from "react-native/Libraries/Utilities/PixelRatio";
-
+import {Image, StyleSheet, Text, View, Dimensions} from "react-native";
+import {Col, Row, Grid} from "react-native-easy-grid";
 
 export default class Moment extends Component {
 
     render() {
+        const {width, height} = Dimensions.get('window');
 
         return (
             <View style={styles.moment_container}>
-                <Text style={styles.moment_header}>
-                    <Image
-                        resizeMode={'contain'}
-                        style={{width: 45, height: 45}}
-                        source={require('../assets/images/dog.jpg')}
-                    />
-                    {this.props.row.item.userName}
-                </Text>
-                <Text style={styles.moment}>
-                    {'data:' + this.props.row.item.content}
+                <Grid>
+                    <Col style={{width: 55, padding: 0}}>
+                        <Image
+                            style={{
+                                flex: 1, height: 45, width: 45, position: 'absolute',
+                                left: 5,
+                                top: 5,
+                                right: 5
+                            }}
+                            resizeMode={'contain'}
+                            source={require('../assets/images/dog.jpg')}
+                        />
+                    </Col>
+                    <Col>
+                        <Row style={{alignItems:'stretch', justifyContent:'center', flex: 1}}>
+                            <Text style={{height:55, fontSize: 22, textAlign:'center'}}>2</Text>
+                        </Row>
+                        <Row>
+                            <Text>3</Text>
+                        </Row>
+                        <Row>
+                            <Text>2</Text>
+                        </Row>
+                        <Row>
+                            <Text>3</Text>
+                        </Row>
+                        <Row>
+                            <Text>2</Text>
+                        </Row>
+                        <Row>
+                            <Text>3</Text>
+                        </Row>
+                        <Row>
+                            <Text>2</Text>
+                        </Row>
+                        <Row>
+                            <Text>3</Text>
+                        </Row>
+                    </Col>
+                </Grid>
+                {/*<Image*/}
+                {/*resizeMode={'contain'}*/}
+                {/*style={{height: height * 0.2, width: height * 0.2}}*/}
+                {/*source={require('../assets/images/dog.jpg')}*/}
+                {/*/>*/}
+                {/*<Text style={styles.moment_header}>*/}
 
-                </Text>
+                {/*{this.props.row.item.userName}*/}
+                {/*</Text>*/}
+                {/*<Text style={styles.moment}>*/}
+                {/*{'data:' + this.props.row.item.content}*/}
+
+                {/*</Text>*/}
             </View>
 
         );
@@ -36,18 +77,13 @@ const styles = StyleSheet.create({
     },
 
     moment_header: {
-        height: 45,
         alignItems: 'center',
         fontSize: 22,
-        backgroundColor: 'black',
 
     },
 
-
-
     moment_container: {
         backgroundColor: 'white',
-        padding: 20,
         borderBottomColor: 'black',
         borderBottomWidth: 1,
     },
